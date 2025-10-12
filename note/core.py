197c58162d6ed2d5f7a4d4001588e400ca301a5c
Soroush Piri Zendedel <soroush_zendedel@Live.com>
2025-10-12 06:06:16 +0330

    Remove unused numpy import and test for cat function

--- note/core.py
+++ note/core.py
@@ -1,6 +1,1 @@
-import numpy as np
-
-
-def cat(arr1, arr2):
-    return np.concatenate([arr1, arr2])
 print("hi!")
--- tests/test_example.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pytest
-
-from note import cat
-
-
-def test_cat():
-    assert np.equal([1, 2, 3, 4], cat([1, 2], [3, 4])).all()
-
